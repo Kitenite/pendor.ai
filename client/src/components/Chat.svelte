89dd84f7408e3b2778f3b3e969ba8bae891e0eb6
Kiet Ho <hoakiet98@gmail.com>
2023-07-22 14:49:24 -0500

    Add chat to client

---
+++ client/src/components/Chat.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { useCompletion } from 'ai/svelte';
+   
+    const { input, handleSubmit, completion } = useCompletion({
+      api: '/api/completion',
+    });
+  </script>
+   
+  <main>
+    <form on:submit={handleSubmit}>
+      <input type="text" bind:value={$input} placeholder="Describe your business..." />
+      <button type="submit">Generate Slogan</button>
+    </form>
+    <p>{$completion}</p>
+  </main>
--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,44 +1,7 @@
-<script lang="ts">
-    import loader from '@monaco-editor/loader';
-    import { onDestroy, onMount } from 'svelte';
-    import type * as Monaco from 'monaco-editor/esm/vs/editor/editor.api';
-
-    let editor: Monaco.editor.IStandaloneCodeEditor;
-    let monaco: typeof Monaco;
-    let editorContainer: HTMLElement;
-
-    onMount(async () => {
-
-        // Remove the next two lines to load the monaco editor from a CDN
-        // see https://www.npmjs.com/package/@monaco-editor/loader#config
-        const monacoEditor = await import('monaco-editor');
-        loader.config({ monaco: monacoEditor.default });
-
-        monaco = await loader.init();
-
-        // Your monaco instance is ready, let's display some code!
-        const editor = monaco.editor.create(editorContainer);
-        const model = monaco.editor.createModel(
-            "console.log('Hello from Monaco! (the editor, not the city...)')",
-            undefined,
-            // Give monaco a hint which syntax highlighting to use
-            monaco.Uri.file('sample.js')
-        );
-        editor.setModel(model);
-    });
-
-    onDestroy(() => {
-        monaco?.editor.getModels().forEach((model) => model.dispose());
-    });
+<script>
+    import CodeEditor from "../components/CodeEditor.svelte";
+    import Chat from "../components/Chat.svelte";
 </script>
 
-<div>
-    <div class="container" bind:this={editorContainer} />
-</div>
-
-<style>
-    .container {
-        width: 100%;
-        height: 600px;
-    }
-</style>
+<Chat />
+<CodeEditor />
