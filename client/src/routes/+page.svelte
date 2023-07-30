cf82536646eb7a14ca41f3057280884b950fdc7f
Kiet Ho <hoakiet98@gmail.com>
2023-07-29 21:21:09 -0500

    Add home page descriptions

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,6 +1,26 @@
 <script>
+	import { Constants } from '$lib';
 	import Header from '../components/Header.svelte';
-	import { goto } from '$app/navigation';
 </script>
 
 <Header />
+
+<div class="max-w-prose mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8 py-6">
+	<h1 class="text-4xl font-extrabold text-center text-gray-900">What is this app?</h1>
+	<p class="mt-2 text-lg text-justify leading-7 pt-4">
+		This app allows you to create web components using AI from scratch or lift it from other
+		websites and edit it with AI. Soon, you'll also be able to create more complex components or
+		even full websites. Go to <a class="font-semibold" href={Constants.CREATE_PAGE}>Create page</a> to
+		get started.
+	</p>
+
+	<ul class="list-disc pl-5 pt-10">
+		<li><strong>Create:</strong> Create and edit components with AI</li>
+		<li>
+			<strong>Collect:</strong> Explore and pull in components from other websites
+		</li>
+		<li>
+			<strong>Components:</strong> View and edit all your saved components
+		</li>
+	</ul>
+</div>
