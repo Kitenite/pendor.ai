cef35a2c38cb32380882177a259a1a64c97e58a9
Kiet Ho <hoakiet98@gmail.com>
2023-08-09 17:40:45 -0500

    Logo name change

--- client/src/components/Logo.svelte
+++ client/src/components/Logo.svelte
@@ -2,5 +2,5 @@
 	<div class="w-5 h-5 rounded-full bg-black">
 		<i class="fas fa-circle text-white" />
 	</div>
-	<p class="font-bold text-xl ml-2">Ged</p>
+	<p class="font-bold text-xl ml-2">Pendor</p>
 </a>
